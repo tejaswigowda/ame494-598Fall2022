1db2f0a690a4d47ed32de23d3c427adf4b60f477
Tejaswi Gowda <tejaswi@asu.edu>
2022-09-29 12:45:19 -0700

    ac

--- ASSIGNMENTS/A4/code/ws-server/server.js
+++ ASSIGNMENTS/A4/code/ws-server/server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const app = express();
 const WebSocket = require('ws')
 
-var MS = require("mongoskin");
-var db = MS.db("mongodb://localhost:27017/sensorData")
 
 
 
@@ -12,11 +10,6 @@ const wss = new WebSocket.Server({ port: 3000})
 wss.on('connection', ws => {
   ws.on('message', message => {
 	var x = `${message}`;
-	  console.log(x)
-	  try{
-		    db.collection("dataWS").insert(JSON.parse(x), function(err, result){
-		    });
-	  }
-	  catch(e){}
+	  console.log(x, new Date());
   })
 })
