0fba7451c7ece38198cb50f8328e2d009cfed960
Tejaswi Gowda <tejaswigowda@users.noreply.github.com>
2022-10-04 13:11:37 -0700

    Update server.js

--- ASSIGNMENTS/A4/code/ws-server/server.js
+++ ASSIGNMENTS/A4/code/ws-server/server.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
 const WebSocket = require('ws')
-
+var lts = {};
 
 const wss = new WebSocket.Server({ port: 3000})
 var count = 0;
 wss.on('connection', ws => {
   ws.on('message', message => {
+	  lts = message;
 	console.log(count, message, new Date());count++;
 	var x = `${message}`;
   })
 })
+
+app.get("getLatest", function(req,res){
+	res.end(JSON.stringify(lts);
+});
+
+
+app.listen(8080);
