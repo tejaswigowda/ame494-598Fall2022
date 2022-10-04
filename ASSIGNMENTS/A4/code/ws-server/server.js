08de87771a59adede71ebb56809aa506656bc98a
Tejaswi Gowda <tejaswigowda@users.noreply.github.com>
2022-10-04 12:20:38 -0700

    Update server.js

--- ASSIGNMENTS/A4/code/ws-server/server.js
+++ ASSIGNMENTS/A4/code/ws-server/server.js
@@ -3,12 +3,11 @@ const app = express();
 const WebSocket = require('ws')
 
 
-
-
 const wss = new WebSocket.Server({ port: 3000})
-
+var count = 0;
 wss.on('connection', ws => {
   ws.on('message', message => {
+	  console.log(count, msg, new Date());count++;
 	var x = `${message}`;
 	  console.log(x, new Date());
   })
