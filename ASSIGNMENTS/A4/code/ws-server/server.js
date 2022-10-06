5393a965111096d9e1f4f76540f23d85b25918fc
Tejaswi Gowda <tejaswi@asu.edu>
2022-10-06 13:10:22 -0700

    ac

--- ASSIGNMENTS/A4/code/ws-server/server.js
+++ ASSIGNMENTS/A4/code/ws-server/server.js
@@ -4,7 +4,6 @@ const WebSocket = require('ws')
 var cors = require('cors');
 
 var lts = {};
-express.static.mime.define({'application/octet-stream': ['csv']})
 
 const wss = new WebSocket.Server({ port: 3000})
 var count = 0;
@@ -26,8 +25,9 @@ app.use(cors())
 app.get("/getLatest.csv", function(req,res){
     var csv = "Time, X, Y, Z \n";
     csv += new Date().toJSON() + "," + lts.x + "," + lts.y + "," + lts.z + "\n";
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('content-type', 'text/csv');
 	res.end(csv);
 });
 
