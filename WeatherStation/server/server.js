07f39faac2216833317dc931b6cfd3cac21fc712
tejaswigowda <tejaswi@foxyninjastudios.com>
2022-10-13 12:40:29 -0700

    ac

--- WeatherStation/server/server.js
+++ WeatherStation/server/server.js
@@ -63,7 +63,7 @@ app.get("/setValue", function (req, res) {
 		h: VALUEh,
 		time: VALUEtime
 	}
-	db.collection("dataWeather").insertOne(dataObj, function(err,result){
+	db.collection("dataWeather").insertOne([dataObj], function(err,result){
     console.log(err,result);
 		console.log("added data: " + JSON.stringify(dataObj));
 	});
